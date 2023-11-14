8a6c02e9731319864e578df01729165c573fc79a
mallareddyjajula <150822039+mallareddyjajula@users.noreply.github.com>
2023-11-14 20:41:49 +0530

    Add files via upload

---
+++ controlflow.py
@@ -0,0 +1,43 @@
+#1. Biggest of two numbers 
+ 
+num1 = float(input("Enter the first number: ")) 
+num2 = float(input("Enter the second number: ")) 
+ 
+if num1 > num2: 
+print(num1," is the biggest number.") 
+elif num2 > num1: 
+print(num2," is the biggest number.") 
+else: 
+print("Both numbers are equal.") 
+ 
+#2. number bw 1 to 100 
+ 
+n=int(input("Enter number: ")) 
+if n>=1 and n<=100: 
+print(n,"number in range: 1-100") 
+else: 
+print("number is not between 1 and 100.") 
+ 
+#3. 0-9 digits program 
+ 
+n=int(input("Enter a number: ")) 
+if n==0: 
+print("zero") 
+elif n==1: 
+print("one") 
+elif n==2: 
+print("two") 
+elif n==3: 
+print("three") 
+elif n==4: 
+print("four") 
+elif n==5: 
+print("five") 
+elif n==6: 
+print("six") 
+elif n==7: 
+print("seven") 
+elif n==8: 
+print("eight") 
+elif n==9: 
+print("nine")
---
+++ oprators.py
@@ -0,0 +1,54 @@
+# OPERATORS:-
+# Arithmetic operator:-
+#   +, -, *, /, % #  /--> Normal Division Operator
+#  //--> Floor Division Operator #  *---> Multiplicatin Operator
+#  **-->Exponential Opertor
+
+a=10
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+
+# Relational Operator/Comparison Operator:-
+# <,<=,>,>,
+"""a=10
+b=20
+print(a<b)
+print(a<=b)
+print(a>b)
+print(a>=b)"""
+
+#str type also
+a='a'
+b='A'
+print(a<b)
+print(a<=b)
+print(a>b)
+print(a>=b)
+
+# Logical Operator:-
+# and,or,not
+# and-->if both argument are True then only result is True
+# or -->if atleast one argument is True then the result is True
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# Ternary operators:
+
+
+
+
+
+
+
+
+
