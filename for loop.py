713385a1812397c7de63cb9fb0c2f891fc602cd6
mallareddyjajula <150822039+mallareddyjajula@users.noreply.github.com>
2023-11-15 19:53:11 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,12 @@
+for i in range(1,100):
+    print(i,end=" ")
+
+for j in range(1,10):
+    if j%2==0:
+        print(j,end=" ")
+
+
+for j in range(1,10):
+    if j%2!=0:
+        print(j,end=" ")
+
