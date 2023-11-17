d126fa6fb54da4a191c80778bc2c93a395a54c9f
mallareddyjajula <150822039+mallareddyjajula@users.noreply.github.com>
2023-11-17 21:08:59 +0530

    Add files via upload

---
+++ arthmetic oparator.py
@@ -0,0 +1,22 @@
+a=10
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a**b)
+
+#arthmetic points in using FloatingPoints
+a=10.5
+b=3.6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+#inditation points and floting points
+a=10
+b=3
+print(a/b)
+print(a//b)
